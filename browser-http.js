9a38c6c717a6fd8be855310b4fd10204bca94d76
Thor Galle <thorgalle@gmail.com>
2020-07-28 19:32:56 +0200

    Add warning about webext environment

--- browser-http.js
+++ browser-http.js
@@ -1,5 +1,5 @@
 function Http() {
-    this.firstRun = true;
+  this.firstRun = true;
 }
 
 /**
@@ -8,105 +8,119 @@ function Http() {
  * (reference: https://developer.chrome.com/extensions/webRequest#life_cycle_footnote)
  * Function from https://stackoverflow.com/questions/4900436/how-to-detect-the-installed-chrome-version
  */
-Http.prototype.getChromeVersion = function() {     
-    var raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
-    return raw ? parseInt(raw[2], 10) : false;
-}
-
-Http.prototype.setCookie = function(cookie) {
-    // no-op
-    // TODO: should this also be set here? -> document.cookie = cookie;
-    // meant for web use
+Http.prototype.getChromeVersion = function () {
+  var raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
+  return raw ? parseInt(raw[2], 10) : false;
 };
 
+Http.prototype.setCookie = function (cookie) {
+  // no-op
+  // TODO: should this also be set here? -> document.cookie = cookie;
+  // meant for web use
+};
 
 /**
- * 
- * @param {*} method 
- * @param {*} url 
+ *
+ * @param {*} method
+ * @param {*} url
  * @param {*} options
  *                  {
  *                      referer: to set a specific referrer header,
  *                      reponseType:        - default document
- *                      credentials: boolean - default true 
- *                  } 
- * @param {*} data for POST or PUT 
+ *                      credentials: boolean - default true
+ *                  }
+ * @param {*} data for POST or PUT
  */
-Http.prototype.http = function(method, url, options, data) {
-
-    /**
-     * Execute an function with a modified Referer header for browser requests
-     * @param {*} requestUrls list of request URL match patters that need a referer change
-     */
-    function setReferrerInterceptor(requestUrls) {
-        function refererListener(details) {
-            const i = details.requestHeaders.findIndex(e => e.name.toLowerCase() == "set-referer");
-            // convert set-referer to Referer
-            if (i != -1) {
-                details.requestHeaders.push({name: "Referer", value: details.requestHeaders[i].value});
-                //delete details.requestHeaders[i]; TODO: this causes problems, the reference is still used above ?
-                // now it's sending
-                // another way that worked is to keep a url -> referer mapping in the object and update it in http
-            }
-            // Firefox uses promises
-            // return Promise.resolve(details);
-            // Chrome doesn't. Todo: https://github.com/mozilla/webextension-polyfill
-        
-            // important: do create a new object, passing the modified argument does not work
-            return {requestHeaders: details.requestHeaders};
-        }
-
-        // modify headers with webRequest hook
-        chrome.webRequest.onBeforeSendHeaders.addListener(
-            refererListener, //  function
-            {urls: requestUrls}, // RequestFilter object
-            ["requestHeaders", "blocking"].concat(this.getChromeVersion() >= 72 ? ["extraHeaders"] : []) //  extraInfoSpec
-        );
-    }
-
-    if (this.firstRun) {
-        setReferrerInterceptor([
-            `${this.URLBASE}/progress/*`,
-            `${this.URLBASE}/lists/byprofile.json`, 
-            `${this.URLBASE}/lists/save.json`,
-            `${this.URLBASE}/lists/delete.json`,
-            `${this.URLBASE}/lists/vocabgrabber/grab.json`,
-            `${this.URLBASE}/lists/load.json`]);
-        this.firstRun = false;
-    }
-
-    return new Promise((resolve, reject) => {
-        let req = new XMLHttpRequest();
-        req.open(method.toUpperCase(), url, true);
-        // headers
-        req.withCredentials = true;
-        req.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-        let sendReg = /PUT|POST/i;
-        if (method.match(sendReg)) {
-            req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-        }
-        // defaults
-        req.responseType = 'document';
-        req.withCredentials = true; 
-        // options
-        if (options) {
-            if (options.referer) {
-                req.setRequestHeader("set-referer", options.referer);
-            }
-            if ( options.responseType ) req.responseType = options.responseType;
-            if ( options.credentials ) req.withCredentials = options.credentials;
-        }
-        // generic response handler
-        req.addEventListener("load", (response) => {
-            resolve(req);
+Http.prototype.http = function (method, url, options, data) {
+  /**
+   * Execute an function with a modified Referer header for browser requests
+   * @param {*} requestUrls list of request URL match patters that need a referer change
+   */
+  function setReferrerInterceptor(requestUrls) {
+    function refererListener(details) {
+      const i = details.requestHeaders.findIndex(
+        (e) => e.name.toLowerCase() == "set-referer"
+      );
+      // convert set-referer to Referer
+      if (i != -1) {
+        details.requestHeaders.push({
+          name: "Referer",
+          value: details.requestHeaders[i].value,
         });
-        // send request
-        if (method.match(sendReg)) {
-            req.send(data);
-        } else {
-            req.send();
-        }
+        //delete details.requestHeaders[i]; TODO: this causes problems, the reference is still used above ?
+        // now it's sending
+        // another way that worked is to keep a url -> referer mapping in the object and update it in http
+      }
+      // Firefox uses promises
+      // return Promise.resolve(details);
+      // Chrome doesn't. Todo: https://github.com/mozilla/webextension-polyfill
+
+      // important: do create a new object, passing the modified argument does not work
+      return { requestHeaders: details.requestHeaders };
+    }
+
+    // modify headers with webRequest hook
+    if (typeof chrome !== "undefined") {
+      chrome.webRequest.onBeforeSendHeaders.addListener(
+        refererListener, //  function
+        { urls: requestUrls }, // RequestFilter object
+        ["requestHeaders", "blocking"].concat(
+          this.getChromeVersion() >= 72 ? ["extraHeaders"] : []
+        ) //  extraInfoSpec
+      );
+    } else {
+      console.warn(`The chrome API is not available. You're probably not running this in the privileged environment of a web extension.
+    In browser projects, this API CAN NOT be used outside of web extensions with the proper permissions. The web extension environment is needed to circumvent CORS and change request headers, both are necessary.`);
+    }
+  }
+
+  if (this.firstRun) {
+    setReferrerInterceptor([
+      `${this.URLBASE}/progress/*`,
+      `${this.URLBASE}/lists/byprofile.json`,
+      `${this.URLBASE}/lists/save.json`,
+      `${this.URLBASE}/lists/delete.json`,
+      `${this.URLBASE}/lists/vocabgrabber/grab.json`,
+      `${this.URLBASE}/lists/load.json`,
+    ]);
+    this.firstRun = false;
+  }
+
+  return new Promise((resolve, reject) => {
+    let req = new XMLHttpRequest();
+    req.open(method.toUpperCase(), url, true);
+    // headers
+    req.withCredentials = true;
+    req.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+    let sendReg = /PUT|POST/i;
+    if (method.match(sendReg)) {
+      req.setRequestHeader(
+        "Content-Type",
+        "application/x-www-form-urlencoded; charset=UTF-8"
+      );
+    }
+    // defaults
+    req.responseType = "document";
+    req.withCredentials = true;
+    // options
+    if (options) {
+      if (options.referer) {
+        req.setRequestHeader("set-referer", options.referer);
+      }
+      if (options.responseType) req.responseType = options.responseType;
+      if (options.credentials) req.withCredentials = options.credentials;
+    }
+    // generic response handler
+    req.addEventListener("load", (response) => {
+      resolve(req);
     });
-}
+    // send request
+    if (method.match(sendReg)) {
+      req.send(data);
+    } else {
+      req.send();
+    }
+  });
+};
 
 module.exports = Http;
--- http.js
+++ http.js
@@ -1,90 +1,87 @@
-const request = require('request-promise');
-const tough = require('tough-cookie');
-const setCookieString = (string, jar, uri) => string.split(';').forEach(single => jar.setCookie(single, uri));
+const request = require("request-promise");
+const tough = require("tough-cookie");
+const setCookieString = (string, jar, uri) =>
+  string.split(";").forEach((single) => jar.setCookie(single, uri));
 
 function Http() {
-    this.cookiejar = null;
+  this.cookiejar = null;
 }
 
-Http.prototype.setCookie = function(cookie) {
-    if (cookie) {
-        if (this.cookiejar === null) this.cookiejar = request.jar();
-        // if (this.cookiejar === null) this.cookiejar = new tough.CookieJar();
-        // setCookieString(cookie, this.cookiejar, 'https://vocabulary.com/');
-        setCookieString(cookie, this.cookiejar, 'https://www.vocabulary.com/');
-    }
+Http.prototype.setCookie = function (cookie) {
+  if (cookie) {
+    if (this.cookiejar === null) this.cookiejar = request.jar();
+    // if (this.cookiejar === null) this.cookiejar = new tough.CookieJar();
+    // setCookieString(cookie, this.cookiejar, 'https://vocabulary.com/');
+    setCookieString(cookie, this.cookiejar, "https://www.vocabulary.com/");
+  }
 };
 
-Http.prototype.getJar = function() {
-    return this.cookiejar;
-}
+Http.prototype.getJar = function () {
+  return this.cookiejar;
+};
 
-Http.prototype.http = function(method, url, options, data) {
-    const self = this;
-    // console.log("cookie this ", this);
-    return new Promise((resolve, reject) => {
-        let requestOptions = {
-            // url,
-            uri: url,
-            method,
-            followAllRedirects: true,
-            jar: self.cookiejar || true,
-            resolveWithFullResponse: true,
-            headers: {}
-        }
+Http.prototype.http = function (method, url, options, data) {
+  const self = this;
+  // console.log("cookie this ", this);
+  return new Promise((resolve, reject) => {
+    let requestOptions = {
+      // url,
+      uri: url,
+      method,
+      followAllRedirects: true,
+      jar: self.cookiejar || true,
+      resolveWithFullResponse: true,
+      headers: {},
+    };
 
-        // console.log("uri: ", requestOptions.uri);
-        // console.log("cookiejar: ", self.cookiejar);
+    // console.log("uri: ", requestOptions.uri);
+    // console.log("cookiejar: ", self.cookiejar);
 
-        let sendReg = /PUT|POST/i;
-        if (method.match(sendReg) && data) {
-            requestOptions['form'] = data;
-        }
-
-        // options
-        if (options) {
-            if (options.referer) {
-                requestOptions.headers['referer'] = options.referer;
-            }
-            // expect json response
-            if ( options.responseType === 'json') requestOptions.json = true;
-            // if ( options.credentials ) req.withCredentials = options.credentials;
-        }
-        
-
-        let transformResponse = (res) => {
-
-            // console.log(requestOptions);
-            // console.log(res.body.length);
-            // console.log(res.headers);
-            // console.log(res.statusCode);
-
-            // documentation: Get the full response instead of just the body
-            
-            let response = {};
-            response.status = res.statusCode;
-
-            // json --> res.response contains data
-            // otherwise res.responseTe
-            if (options && options.responseType === 'json') {
-                response.response = res.body;
-            } else {
-                // TODO: also populate responsetext? confusing
-                response.responseText = res.body;
-                response.response = res.body;
-            }
-
-            //console.log(res.body);
-            return response;
-        }
-
-        let callTransformed = (f) => (a => f(transformResponse(a)));
-        request(requestOptions)
-        .then(callTransformed(resolve))
-        .catch(callTransformed(reject));
+    let sendReg = /PUT|POST/i;
+    if (method.match(sendReg) && data) {
+      requestOptions["form"] = data;
     }
 
-    )
-}
+    // options
+    if (options) {
+      if (options.referer) {
+        requestOptions.headers["referer"] = options.referer;
+      }
+      // expect json response
+      if (options.responseType === "json") requestOptions.json = true;
+      // if ( options.credentials ) req.withCredentials = options.credentials;
+    }
+
+    let transformResponse = (res) => {
+      // console.log(requestOptions);
+      // console.log(res.body.length);
+      // console.log(res.headers);
+      // console.log(res.statusCode);
+
+      // documentation: Get the full response instead of just the body
+
+      let response = {};
+      response.status = res.statusCode;
+
+      // json --> res.response contains data
+      // otherwise res.responseTe
+      if (options && options.responseType === "json") {
+        response.response = res.body;
+      } else {
+        // TODO: also populate responsetext? confusing
+        response.responseText = res.body;
+        response.response = res.body;
+      }
+
+      //console.log(res.body);
+      return response;
+    };
+
+    let callTransformed = (f) => (a) => f(transformResponse(a));
+    request(requestOptions)
+      .then(callTransformed(resolve))
+      .catch(callTransformed(reject));
+  });
+};
 
 module.exports = Http;
